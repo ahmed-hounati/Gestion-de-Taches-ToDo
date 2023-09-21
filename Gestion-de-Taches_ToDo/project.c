bcd320f4bab0396acdb8d3c9110477f36446d2a3
hounati <hounatiahmed@gmail.com>
2023-09-21 17:43:42 +0100

    update

--- Gestion-de-Taches_ToDo/project.c
+++ Gestion-de-Taches_ToDo/project.c
@@ -36,8 +36,26 @@ void ajouter(tache tch[])
     printf("Veuillez entrer la description : ");
     scanf(" %[^\n]", tch[taille].description);
 
-    printf("Veuillez entrer le statut : ");
-    scanf(" %[^\n]", tch[taille].statut);
+    int choix;
+    printf("1 - Si votre tach est a realiser \n");
+    printf("2 - Si votre tache est a en cours de realisation \n");
+    printf("3 - Si votre tache est a en finalisee \n");
+    printf("votre choix est : ");
+    scanf("%d", &choix);
+    switch (choix)
+    {
+    case 1:
+        strcpy(tch[taille].statut, " est a realiser \n");
+        break;
+    case 2:
+        strcpy(tch[taille].statut, " est a en cours de réalisation \n");
+        break;
+    case 3:
+        strcpy(tch[taille].statut, " est a finalisee \n");
+        break;
+    default:
+        break;
+    }
 
     printf("Entrer le jour : ");
     scanf("%d", &tch[taille].deadline.jour);
@@ -58,6 +76,21 @@ void ajouterTaches(tache tch[], int nombreTaches)
         ajouter(tch);
     }
 }
+
+void affichages(tache tch[])
+{
+    int i;
+    for (i = 0; i < taille; i++)
+    {
+        printf("\t\t\t\t\t------Tache %d---- \n", tch[i].id);
+        printf("\t\t\t\t\tId : %d \n", tch[i].id);
+        printf("\t\t\t\t\tTitre : %s \n", tch[i].titre);
+        printf("\t\t\t\t\tDescription : %s \n", tch[i].description);
+        printf("\t\t\t\t\tStatut : %s \n", tch[i].statut);
+        printf("\t\t\t\t\tLa date  %d/%d/%d \n", tch[i].deadline.jour, tch[i].deadline.mois, tch[i].deadline.annee);
+    }
+}
+
 void trie(tache tch[])
 {
     int cmp;
@@ -76,19 +109,84 @@ void trie(tache tch[])
         }
     } while (cmp);
 }
-
-void affichages(tache tch[])
+int compareDates(date date1, date date2)
 {
-    int i;
-    for (i = 0; i < taille; i++)
+    if (date1.annee != date2.annee)
     {
-        printf("\t\t\t\t\t----Votre taches-----\n");
-        printf("\t\t\t\t\t------Tache %d---- \n", tch[i].id);
-        printf("\t\t\t\t\tId : %d \n", tch[i].id);
-        printf("\t\t\t\t\tTitre : %s \n", tch[i].titre);
-        printf("\t\t\t\t\tDescription : %s \n", tch[i].description);
-        printf("\t\t\t\t\tStatut : %s \n", tch[i].statut);
-        printf("\t\t\t\t\tLa date  %d/%d/%d \n", tch[i].deadline.jour, tch[i].deadline.mois, tch[i].deadline.annee);
+        return date1.annee - date2.annee;
+    }
+    else if (date1.mois != date2.mois)
+    {
+        return date1.mois - date2.mois;
+    }
+    else
+    {
+        return date1.jour - date2.jour;
+    }
+}
+
+void triedead(tache T[])
+{
+    tache tmp;
+    for (int i = 0; i < taille - 1; i++)
+    {
+        for (int j = 0; j < taille - i - 1; j++)
+        {
+            if (compareDates(T[j].deadline, T[j + 1].deadline) > 0)
+            {
+                tmp = T[j];
+                T[j] = T[j + 1];
+                T[j + 1] = tmp;
+            }
+        }
+    }
+}
+
+void modifierDesc(tache tch[])
+{
+    int id;
+    printf("Veuller entrer l Id que vous voulez modifier : ");
+    scanf("%d", &id);
+    for (int i = 0; i < taille; i++)
+    {
+        if (tch[i].id == id)
+        {
+            printf("Taper la nouvelle description de la tache :");
+            scanf(" %[^\n]", tch[i].description);
+        }
+    }
+}
+
+void modifierStatut(tache tch[])
+{
+    int id;
+    printf("Veuller entrer l Id que vous voulez modifier : ");
+    scanf("%d", &id);
+    for (int i = 0; i < taille; i++)
+    {
+        if (tch[i].id == id)
+        {
+            printf("Taper la nouvelle statut de la tache :");
+            scanf(" %[^\n]", tch[i].statut);
+        }
+    }
+}
+void modifierdead(tache tch[])
+{
+    int id;
+    printf("Veuller entrer l Id que vous voulez modifier : ");
+    scanf("%d", &id);
+    for (int i = 0; i < taille; i++)
+    {
+        if (tch[i].id == id)
+        {
+            printf("Taper la nouvelle jour :");
+            scanf("%d", tch[i].deadline.jour);
+            printf("Taper la nouvelle mois : ");
+            scanf("%d", tch[i].deadline.mois);
+            printf("Taper la nouvelle annee :");
+            scanf("%d", tch[i].deadline.annee);
+        }
     }
 }
 int main()
@@ -144,15 +242,20 @@ int main()
                     printf("\t\t\t\t\tTaches triees par titre :\n");
                     for (int i = 0; i < taille; i++)
                     {
-                        printf("Tache %d : %s\n", tch[i].id, tch[i].titre);
+                        affichages(tch);
                     }
                     break;
                 case 2:
                     printf("\t\t\t\t\tTrier les tache par deadline.\n");
+                    triedead(tch);
+                    affichages(tch);
+                    break;
                 case 3:
                     printf("\t\t\t\t\tAfficher les taches dont le deadline est dans 3 jours ou moins.\n");
+                    break;
                 case 4:
                     printf("\t\t\t\t\tQuitter.\n");
+                    break;
                 default:
                     printf("choix invalid.\n");
                     break;
@@ -166,21 +269,27 @@ int main()
             do
             {
                 printf("\t\t\t\t\t1 - Modifier la description d une tache.\n");
-                printf("2 - Modifier la statut d une tache.\n");
-                printf("3 - Modifier la deadline d une tache.\n");
-                printf("4 - Quitter \n");
+                printf("\t\t\t\t\t2 - Modifier la statut d une tache.\n");
+                printf("\t\t\t\t\t3 - Modifier la deadline d une tache.\n");
+                printf("\t\t\t\t\t4 - Quitter \n");
                 printf("Choisi le modifier que vous voulez : ");
                 scanf("%d", &modifier);
                 switch (modifier)
                 {
                 case 1:
                     printf("Modifier la description d une tache.\n");
+                    modifierDesc(tch);
+                    affichages(tch);
                     break;
                 case 2:
-                    printf("Modifier la description d une tache.\n");
+                    printf("Modifier le statut d une tache.\n");
+                    modifierStatut(tch);
+                    affichages(tch);
                     break;
                 case 3:
-                    printf("Modifier la description d une tache.\n");
+                    printf("Modifier la deadline d une tache.\n");
+                    modifierdead(tch);
+                    affichages(tch);
                     break;
                 case 4:
                     printf("Quitter \n");
@@ -254,7 +363,7 @@ int main()
             break;
         case 8:
             printf("\t\t\t\t\tQuitter \n");
-
+            break;
         default:
             printf("Choix invalide \n");
             break;
